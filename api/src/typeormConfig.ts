52e79a924800bf81e8e0fdb333dd70dee07ec574
varshildoshi <varshil.mscit16@gmail.com>
2024-11-30 18:43:59 +0530

    new commit

--- api/src/typeormConfig.ts
+++ api/src/typeormConfig.ts
@@ -2,11 +2,20 @@ import { TypeOrmModuleOptions } from "@nestjs/typeorm";
 
 export const typeOrmConfig: TypeOrmModuleOptions = {
     type: 'postgres',
-    username: 'postgres',
-    password: 'root',
-    port: 5432,
-    host: 'localhost',
-    database: 'blogapp',
+    username: process.env.PGUSER,
+    password: process.env.PGPASSWORD,
+    port: parseInt(process.env.PGPORT),
+    host: process.env.PGHOST,
+    database: process.env.POSTGRES_DB,
     synchronize: true,
     entities: ["dist/**/*.entity{.ts,.js}"],
+
+    // type: 'postgres',
+    // username: 'postgres',
+    // password: 'root',
+    // port: 5432,
+    // host: 'localhost',
+    // database: 'blogapp',
+    // synchronize: true,
+    // entities: ["dist/**/*.entity{.ts,.js}"],
 };
--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -2,7 +2,8 @@
 // `ng build` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
 
-const devBaseURL = 'http://localhost:3000/';
+// const devBaseURL = 'http://localhost:3000/';
+const devBaseURL = 'https://blog-production-b112.up.railway.app/';
 
 export const environment = {
   production: false,
