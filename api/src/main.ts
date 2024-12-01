bba61716681cd16767d5ac5a159a4ba5d72a31a8
varshildoshi <varshil.mscit16@gmail.com>
2024-12-01 15:34:34 +0530

    new commit

--- api/src/main.ts
+++ api/src/main.ts
@@ -7,7 +7,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.use(bodyParser.json({ limit: '50mb' }));
   app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
-  app.enableCors();
+  // app.enableCors();
   app.enableCors({
     origin: ['http://localhost:3000/', 'https://blog-web.up.railway.app/'], 
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE', 
