2b213b28002f9d9cb1dd91cf0688bfd52eb7d865
varshildoshi <varshil.mscit16@gmail.com>
2024-12-01 15:26:33 +0530

    new commit

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -2,8 +2,8 @@
 // `ng build` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
 
-const devBaseURL = 'http://localhost:3000/';
-// const devBaseURL = 'https://blog-web.up.railway.app/';
+// const devBaseURL = 'http://localhost:3000/';
+const devBaseURL = 'https://blog-web.up.railway.app/';
 
 export const environment = {
   production: false,
